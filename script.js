fe1976c527d75c5d5e283a7a45e16825c97a68af
Borjan40 <andrey40cadrus@gmail.com>
2023-11-29 22:02:45 +0300

    рабочий код из урока лущенко

--- script.js
+++ script.js
@@ -1,66 +1,43 @@
+document.addEventListener('touchstart', handleTouchStart, false)
+document.addEventListener('touchmove', handleTouchMove, false)
+
+const logBlock = document.querySelector('.log-block');
+
 let x1 = null
 let y1 = null
 
 function handleTouchStart(event) {
-    const firstTouch = event.touches[0]
+    const firstTouch = event.touches[0];
     x1 = firstTouch.clientX
     y1 = firstTouch.clientY
-
-    // console.log("touch x1:", x1, "touch y1:", y1)
-    console.log("touch", "x1:", x1, "y1:", y1, event)
+    // console.log(Boolean((!x1 || !y1)))
+    // console.log("touch", "x1:", x1, "y1:", y1, event)
 }
 
 function handleTouchMove(event) {
-    // console.log(event)
-    // const firstTouch = event.touches[0]
-    // x1 = "ppp"
-    // setTimeout(() => console.log(x1, "vvs"), 1000
-
-    // if (!y1 || !x1) {
-    //     return console.log("if")
-    // }
-
-    // else {
-    //     return console.log("else")
-    // }
-
-
-    // console.log("move x1:", x1, "move y1:", y1)
-    console.log("move", "X:", event.touches[0].clientX, "Y:", event.touches[0].clientY, event)
-
-    // let move = event
-    // return move
-
-    let arrKey = [];
-    let arrVal = [];
-
-    for (let key in event) {
-        arrKey.push(key)
-        arrVal.push(event[key])
+    if (!x1 || !y1) {
+        return false;
     }
+    let x2 = event.touches[0].clientX
+    let y2 = event.touches[0].clientY
+    let diffX = x2 - x1;
+    let diffY = y2 - y1;
+    console.log(x1)
 
-    console.log(arrVal)
-    let box = document.querySelector(".log-block")
-    // console.log(testDEtr)
+    console.log('x1:', x1, 'x2:', x2, 'diffX:', diffX, 'y1:', y1, 'y2:', y2, 'diffY:', diffY)
 
-    const objString = Object.assign({}, ...arrKey.map((n, i) => ({ [n]: arrVal[i].toString() })))
-    console.log("objString", objString)
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+        // right - left
+        if (diffX > 0) (logBlock.textContent = "right")
+        else (logBlock.textContent = "left")
+    }
+    else {
+        // top - bottom
+        if (diffY > 0) (logBlock.textContent = "bottom")
+        else (logBlock.textContent = "top")
+    }
+    x1 = null
+    y1 = null
 
-    const obj = Object.assign({}, ...arrKey.map((n, i) => ({ [n]: arrVal[i] })))
-    console.log("obj", obj)
-
-
-
-    // const [...info] = obj
-    // console.log(info)
-    // box.innerHTML = info
 }
 
-
-const logBlock = document.querySelector('.log-block')
-
-document.addEventListener('touchstart', handleTouchStart, false)
-document.addEventListener('touchmove', handleTouchMove, false)
-
-
-// function handleTouchMove(event)
